59b5a5550cd93b7fc0d85ef482ea625fd7aded0c
MJ Daliva <95694408+Synesthesiaaa@users.noreply.github.com>
2026-04-22 07:51:23 +0800

    .

--- config/horizon.php
+++ config/horizon.php
@@ -196,50 +196,54 @@ return [
     |
     */
 
-    'defaults' => [
+  'defaults' => [
+    'supervisor-1' => [
+        'connection' => 'redis',
+        'queue' => ['default'],
+        'balance' => 'auto',
+        'autoScalingStrategy' => 'time',
+        'maxProcesses' => 1,
+        'maxTime' => 0,
+        'maxJobs' => 0,
+        'memory' => 128,
+        'tries' => 1,
+        'timeout' => 60,
+        'nice' => 0,
+    ],
+
+    'supervisor-imports' => [
+        'connection' => 'redis',
+        'queue' => ['imports'],
+        'balance' => 'simple',
+        'maxProcesses' => 2,
+        'memory' => 512,     // XLSX parsing can be heavy
+        'tries' => 2,
+        'timeout' => 1800,   // match ImportLeadsFileJob::$timeout
+        'nice' => 0,
+    ],
+],
+
+'environments' => [
+    'production' => [
         'supervisor-1' => [
-            'connection' => 'redis',
-            'queue' => ['default'],
-            'balance' => 'auto',
-            'autoScalingStrategy' => 'time',
-            'maxProcesses' => 1,
-            'maxTime' => 0,
-            'maxJobs' => 0,
-            'memory' => 128,
-            'tries' => 1,
-            'timeout' => 60,
-            'nice' => 0,
+            'maxProcesses' => 10,
+            'balanceMaxShift' => 1,
+            'balanceCooldown' => 3,
         ],
         'supervisor-imports' => [
-            'connection' => 'redis',
-            'queue' => ['imports'],
-            'balance' => 'simple',
             'maxProcesses' => 2,
-            'memory' => 512,     // XLSX parsing can be heavy
-            'tries' => 2,
-            'timeout' => 1800,   // match ImportLeadsFileJob::$timeout
-            'nice' => 0,
         ],
     ],
 
-
-    
-    'environments' => [
-        'production' => [
-            'supervisor-1' => [
-                'maxProcesses' => 10,
-                'balanceMaxShift' => 1,
-                'balanceCooldown' => 3,
-            ],
+    'local' => [
+        'supervisor-1' => [
+            'maxProcesses' => 3,
         ],
-
-        'local' => [
-            'supervisor-1' => [
-                'maxProcesses' => 3,
-            ],
+        'supervisor-imports' => [
+            'maxProcesses' => 1,
         ],
     ],
-
+],
     /*
     |--------------------------------------------------------------------------
     | File Watcher Configuration
